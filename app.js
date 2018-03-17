454e504860eba9ef735599f17172d8adeba96f1a
dakshkohli23 <dakshkohli23@gmail.com>
2018-03-17 23:59:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Load up the discord.js library
-const Discord = require("discord.js");
+var Discord = require("discord.js");
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
-const client = new Discord.Client();
+var client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./config.json");
+var config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
@@ -36,15 +36,15 @@ client.on("message", async message => {
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  var command = args.shift().toLowerCase();
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
   if(command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
+    var m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     m.edit(`Hello Mr. Dlaize`);
   }
@@ -52,7 +52,7 @@ client.on("message", async message => {
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
+    var sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
@@ -60,7 +60,7 @@ client.on("message", async message => {
   }
   
 if(command === "pika") {
-    const m = await message.channel.send("Wait");
+    var m = await message.channel.send("Wait");
     m.edit(`Pikachu`);
   } 
 
@@ -73,12 +73,12 @@ if(command === "friday") {
 }
 
 if (command === "listemojis") {
-  const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
+  var emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
   message.channel.send(emojiList);
 }
 
 if(command === "th9") {
-   const th9 = client.emojis.get("409429217298612235");
+   var th9 = client.emojis.get("409429217298612235");
    message.channel.send(`${th9}`);
 }
 
@@ -90,7 +90,8 @@ if(command === "embed") {
 }
 
 if(command === "image") {
-	const embed = new Discord.RichEmbed()
+	
+var embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
   .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
   /*

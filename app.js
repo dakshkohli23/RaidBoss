38dc44b4aee11f5452646522fdafb54876277e6a
dakshkohli23 <dakshkohli23@gmail.com>
2018-03-18 15:19:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ var embed = new Discord.RichEmbed()
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
-  .setColor(01B050)
+  .setColor("#01B050")
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "https://i.imgur.com/r1UJqlE.png")
   .setImage("https://i.imgur.com/2ANI7WN.gif")

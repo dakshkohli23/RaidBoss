5048bac1270fc85138ced382a351c990dad14b29
dakshkohli23 <dakshkohli23@gmail.com>
2018-03-18 15:53:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,19 +73,15 @@
 	   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
 	   */
 	  .setColor("#01B050")
-	  .setDescription("**TYPE:**")
+	  .setDescription("**🚩 TYPE:** Grass/Poison")
 	  .setFooter("This is the footer text, it can hold 2048 characters", "https://i.imgur.com/r1UJqlE.png")
 	  .setImage("https://i.imgur.com/2ANI7WN.gif")
 	  .setThumbnail("https://i.imgur.com/r1UJqlE.png")
-	  .addField("MAX CP : 981")
+	  .addField("**🔼 MAX CP :**", " 981")
 	  /*
 	   * Inline fields may not display as inline if the thumbnail and/or image is too big.
 	   */
-	  .addField("Inline Field", "They can also be inline.", true)
-	  /*
-	   * Blank field, useful to create some space.
-	   */
-	  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+	  .addField("**WEATHER BOOST**", "☀ and ☁")
 
 	  message.channel.send({embed});
 	}

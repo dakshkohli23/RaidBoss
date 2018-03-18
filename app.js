5a4be449a4bf2d22d0d45d575f3a88f520432978
dakshkohli23 <dakshkohli23@gmail.com>
2018-03-18 15:39:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,95 +1,95 @@
-// Import the discord.js module
-var Discord = require("discord.js");
+	// Import the discord.js module
+	var Discord = require("discord.js");
 
-// Create an instance of a Discord client
-var client = new Discord.Client();
+	// Create an instance of a Discord client
+	var client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
-var config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+	// Here we load the config.json file that contains our token and our prefix values. 
+	var config = require("./config.json");
+	// config.token contains the bot's token
+	// config.prefix contains the message prefix.
 
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-});
-
-
+	client.on("ready", () => {
+	  // This event will run if the bot starts, and logs in, successfully.
+	  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+	  // Example of changing the bot's playing game to something useful. `client.user` is what the
+	  // docs refer to as the "ClientUser".
+	});
 
 
-client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  var command = args.shift().toLowerCase();
-  
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  
-  if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    var m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    m.edit(`Hello Mr. Dlaize`);
-  }
-  
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    var sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
-  
 
-if(command === "embed") {
-   message.channel.sendMessage({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
-}});
-}
 
-if(command === "image") {
-	
-var embed = new Discord.RichEmbed()
-  .setTitle("Bulbasaur")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor("#01B050")
-  .setDescription("**TYPE:**")
-  .setFooter("This is the footer text, it can hold 2048 characters", "https://i.imgur.com/r1UJqlE.png")
-  .setImage("https://i.imgur.com/2ANI7WN.gif")
-  .setThumbnail("https://i.imgur.com/r1UJqlE.png")
-  .addField("MAX CP : 981","")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+	client.on("message", async message => {
+	  // This event will run on every single message received, from any channel or DM.
+	  
+	  // It's good practice to ignore other bots. This also makes your bot ignore itself
+	  // and not get into a spam loop (we call that "botception").
+	  if(message.author.bot) return;
+	  
+	  // Also good practice to ignore any message that does not start with our prefix, 
+	  // which is set in the configuration file.
+	  if(message.content.indexOf(config.prefix) !== 0) return;
+	  
+	  // Here we separate our "command" name, and our "arguments" for the command. 
+	  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+	  // command = say
+	  // args = ["Is", "this", "the", "real", "life?"]
+	  var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	  var command = args.shift().toLowerCase();
+	  
+	  // Let's go with a few common example commands! Feel free to delete or change those.
+	  
+	  if(command === "ping") {
+		// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+		var m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+		m.edit(`Hello Mr. Dlaize`);
+	  }
+	  
+	  if(command === "say") {
+		// makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+		// To get the "message" itself we join the `args` back into a string with spaces: 
+		var sayMessage = args.join(" ");
+		// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+		message.delete().catch(O_o=>{}); 
+		// And we get the bot to say the thing: 
+		message.channel.send(sayMessage);
+	  }
+	  
 
-  message.channel.send({embed});
-}
+	if(command === "embed") {
+	   message.channel.sendMessage({embed: {
+	  color: 3447003,
+	  description: "A very simple Embed! (message.author.toString())"
+	}});
+	}
 
-});
+	if(command === "image") {
+		
+	var embed = new Discord.RichEmbed()
+	  .setTitle("Bulbasaur")
+	  /*
+	   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+	   */
+	  .setColor("#01B050")
+	  .setDescription("**TYPE:**")
+	  .setFooter("This is the footer text, it can hold 2048 characters", "https://i.imgur.com/r1UJqlE.png")
+	  .setImage("https://i.imgur.com/2ANI7WN.gif")
+	  .setThumbnail("https://i.imgur.com/r1UJqlE.png")
+	  .addField("MAX CP : 981","")
+	  /*
+	   * Inline fields may not display as inline if the thumbnail and/or image is too big.
+	   */
+	  .addField("Inline Field", "They can also be inline.", true)
+	  /*
+	   * Blank field, useful to create some space.
+	   */
+	  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
 
-client.login(config.token);
+	  message.channel.send({embed});
+	}
+
+	});
+
+	client.login(config.token);
